e011847c4d05ce0fc8284881cba0578a650d1d51
mbravosarria <mbravosarria@gmail.com>
2023-04-11 00:10:32 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+  const date = new Date()
+  res.send({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  })
+})
+
+app.get("/:date", (req, res) => {
+  try {
+    const date = new Date(
+      req.params.date.indexOf("-") !== -1
+        ? req.params.date
+        : parseInt(req.params.date)
+    )
+    res.send({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    })
+  } catch (error) {
+    res.send({ error: "Invalid Date" })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Timestamp microservice listening on port ${port}`)
+})
