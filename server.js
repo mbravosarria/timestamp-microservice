a46ea5b73bcefc638e3841a4f3d185a1f4541b49
mbravosarria <mbravosarria@gmail.com>
2023-04-11 11:36:59 -0400

    fix: remove cors

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express")
-const cors = require("cors")
 const path = require("path")
 
 const app = express()
 const port = 3000
-
-app.use(cors({ optionsSuccessStatus: 200 }))
 
 app.use(express.static(path.join(__dirname, "public")))
 
